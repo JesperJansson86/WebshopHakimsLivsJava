569c0c9b76bd2c26a4abf7aac7c1d039eb059f2e
kdhedman <kenth.david.hedman@gmail.com>
2021-04-16 13:32:45 +0200

    inital commit #nånting

---
+++ src/main/java/com/example/hakimlivs/HakimLivsApplication.java
@@ -0,0 +1,13 @@
+package com.example.hakimlivs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HakimLivsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HakimLivsApplication.class, args);
+    }
+
+}
