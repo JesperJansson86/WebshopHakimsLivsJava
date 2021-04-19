b21c4db41494d51bf2c35e96001855f08645867c
kdhedman <kenth.david.hedman@gmail.com>
2021-04-19 09:52:55 +0200

    några Controllers & Repos tillagt

---
+++ src/main/java/com/example/hakimlivs/repositories/AreaCodeRepository.java
@@ -0,0 +1,8 @@
+package com.example.hakimlivs.repositories;
+
+import com.example.hakimlivs.models.AreaCode;
+import org.springframework.data.repository.CrudRepository;
+
+
+public interface AreaCodeRepository  extends CrudRepository<AreaCode, Long> {
+}
---
+++ src/main/java/com/example/hakimlivs/repositories/BrandRepository.java
@@ -0,0 +1,14 @@
+package com.example.hakimlivs.repositories;
+
+import com.example.hakimlivs.models.Brand;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by David Hedman <br>
+ * Date: 2021-04-19 <br>
+ * Time: 09:30 <br>
+ * Project: hakimLivs <br>
+ * Copyright: Nackademin <br>
+ */
+public interface BrandRepository extends CrudRepository<Brand, Long> {
+}
