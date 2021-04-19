7d93aab1ac58ac4c4195f616832b804d9f01c354
kdhedman <kenth.david.hedman@gmail.com>
2021-04-19 09:52:44 +0200

    Changes by Hanna Edlund några Controllers & Repos tillagt

---
+++ src/main/java/com/example/hakimlivs/controllers/ProductController.java
@@ -0,0 +1,43 @@
+package com.example.hakimlivs.controllers;
+
+import com.example.hakimlivs.models.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by David Hedman <br>
+ * Date: 2021-04-19 <br>
+ * Time: 09:38 <br>
+ * Project: hakimLivs <br>
+ * Copyright: Nackademin <br>
+ */
+@RestController
+@RequestMapping(path = ("/api/product"))
+public class ProductController {
+
+    @GetMapping(path = "/add")
+    public String addProduct(
+            @RequestParam String title,
+            @RequestParam String description,
+            @RequestParam double price,
+            @RequestParam int inventory,
+            @RequestParam int quantity,
+            @RequestParam int size,
+            @RequestParam Brand brand,
+            @RequestParam Category category,
+            @RequestParam Unit unit,
+            @RequestParam boolean visibility
+    ){
+        Product p = new Product();
+        p.setTitle(title);
+        p.setDescription(description);
+        p.setPrice(price);
+        p.setInventory(inventory);
+        p.setQuantity(quantity);
+        c.setAddress(addressrepo.findById(address).get());
+
+        return String.format("%s has been added", title);
+    }
+}
