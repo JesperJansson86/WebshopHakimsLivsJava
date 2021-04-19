e13e2aafb96f7f551136de364eef0757e5e3d0d4
Luar2001 <Lukas.Aronsson@protonmail.com>
2021-04-19 12:07:22 +0200

    added Some constructors to some model objects (@Data only works if fields are @NonNull)

--- src/main/java/com/example/hakimlivs/models/City.java
+++ src/main/java/com/example/hakimlivs/models/City.java
@@ -1,7 +1,12 @@
 package com.example.hakimlivs.models;
 
 import lombok.Data;
-import javax.persistence.*;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 /**
  * Created by Lukas Aronsson
@@ -13,6 +18,7 @@ import javax.persistence.*;
 
 @Data
 @Entity
+@NoArgsConstructor
 public class City {
 
     @Id
@@ -20,4 +26,8 @@ public class City {
     private Long id;
 
     private String city;
+
+    public City(String city) {
+        this.city = city;
+    }
 }
