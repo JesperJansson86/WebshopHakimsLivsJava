0d87817afd544f505554b769cb6af13ce4e791d5
Luar2001 <Lukas.Aronsson@protonmail.com>
2021-04-19 12:07:56 +0200

    Created UnitController based on how CustomerController was made

--- src/main/java/com/example/hakimlivs/controllers/UnitController.java
+++ src/main/java/com/example/hakimlivs/controllers/UnitController.java
@@ -2,6 +2,7 @@ package com.example.hakimlivs.controllers;
 
 import com.example.hakimlivs.models.Unit;
 import com.example.hakimlivs.repositories.UnitRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(path = ("/api/unit"))
 public class UnitController {
+    @Autowired
     UnitRepository unitRepository;
 
     @GetMapping(path = "/add")
