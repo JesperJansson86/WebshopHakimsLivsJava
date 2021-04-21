c0570398f279af8ba6c1c8c91515dd78bca91257
Jesper Jansson <jesper.jansson@yh.nackademin.se>
2021-04-21 09:44:15 +0200

    Heroku connection credentials.

--- src/main/java/com/example/hakimlivs/controllers/Order_ContainsController.java
+++ src/main/java/com/example/hakimlivs/controllers/Order_ContainsController.java
@@ -1,4 +1,39 @@
 package com.example.hakimlivs.controllers;
 
+
+import com.example.hakimlivs.models.Order_Contains;
+import com.example.hakimlivs.repositories.Order_ContainsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping(path = ("/api/order_Contains"))
 public class Order_ContainsController {
+    @Autowired
+    Order_ContainsRepository Order_ContainsRepository;
+
+    @GetMapping(path = "/add")
+    public String addorder_Contains(
+            @RequestParam String order_Contains
+    ){
+        Order_Contains order_c = new Order_Contains();
+
+        Order_ContainsRepository.save(order_c);
+        return "\norder_Contains has been added! ";
+    }
+
+    @GetMapping(path ="/byId")
+    public Order_Contains getorder_ContainsById(@RequestParam Long id){
+        return Order_ContainsRepository.findById(id).get();
+    }
+
+    @GetMapping(path = "/all")
+    public Iterable<Order_Contains> getAllorder_Contains(){
+        return Order_ContainsRepository.findAll();
+    }
+
 }
