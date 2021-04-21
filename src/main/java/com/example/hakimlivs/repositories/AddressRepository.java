21b2e71feee1019b48fa5e652f49b6e3f44ef3f6
Luar2001 <Lukas.Aronsson@protonmail.com>
2021-04-21 13:34:13 +0200

    Added findAddressById

--- src/main/java/com/example/hakimlivs/repositories/AddressRepository.java
+++ src/main/java/com/example/hakimlivs/repositories/AddressRepository.java
@@ -1,9 +1,10 @@
 package com.example.hakimlivs.repositories;
 
 import com.example.hakimlivs.models.Address;
-import com.example.hakimlivs.models.AreaCode;
 import org.springframework.data.repository.CrudRepository;
 
 public interface AddressRepository extends CrudRepository<Address, String> {
     public Address findAddressByAddress(String a);
+
+    Address findAddressById(long addressId);
 }
