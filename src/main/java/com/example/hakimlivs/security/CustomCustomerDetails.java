fdf0b3da4ff54fde00ac4863b50a1c7d531813eb
Hanna Edlund <hanna@edlunds.biz>
2021-09-07 11:19:37 +0200

    added security classes

---
+++ src/main/java/com/example/hakimlivs/security/CustomCustomerDetails.java
@@ -0,0 +1,70 @@
+package com.example.hakimlivs.security;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.example.hakimlivs.models.Customer;
+
+import java.util.Collection;
+import java.util.Collections;
+
+/**
+ * Created by Hanna Edlund
+ * Date: 2021-09-07
+ * Time: 10:50
+ * Project: Java
+ */
+public class CustomCustomerDetails implements UserDetails{
+
+    private Customer customer;
+
+    public CustomCustomerDetails(Customer customer) {
+        super();
+        this.customer = customer;
+    }
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        String customerRole = "Customer";
+        String adminRole = "Admin";
+        String role;
+
+        if(customer.getAdminStatus()){
+            role = adminRole;
+        }
+        else role = customerRole;
+        return Collections.singleton(new SimpleGrantedAuthority(role));
+    }
+
+    @Override
+    public String getPassword() {
+        return customer.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return customer.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
---
+++ src/main/java/com/example/hakimlivs/security/CustomCustomerDetailsService.java
@@ -0,0 +1,32 @@
+package com.example.hakimlivs.security;
+
+import com.example.hakimlivs.models.Customer;
+import com.example.hakimlivs.repositories.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by Hanna Edlund
+ * Date: 2021-09-07
+ * Time: 10:50
+ * Project: Java
+ */
+@Service
+public class CustomCustomerDetailsService implements UserDetailsService{
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Customer customer = customerRepository.findCustomerByEmail(username);
+        if(customer == null){
+            throw new UsernameNotFoundException("User Not Found");
+        }
+        return new CustomCustomerDetails(customer);
+    }
+}
