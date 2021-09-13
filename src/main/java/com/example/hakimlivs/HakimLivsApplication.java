eec20aba0ea3ac530b33104feb63ebb67f82bee8
kdhedman <kenth.david.hedman@gmail.com>
2021-09-13 09:44:18 +0200

    Swagger Merge

--- src/main/java/com/example/hakimlivs/HakimLivsApplication.java
+++ src/main/java/com/example/hakimlivs/HakimLivsApplication.java
@@ -1,13 +1,8 @@
 package com.example.hakimlivs;
 
-import com.example.hakimlivs.security.jwtToken.filter.SecretKeeper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
 
 @EnableSwagger2
 @SpringBootApplication
