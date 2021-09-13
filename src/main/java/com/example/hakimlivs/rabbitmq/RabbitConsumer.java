de40a307313a9982653588b3741487b77274cc65
Jesper Jansson <jesper.jansson@yh.nackademin.se>
2021-09-13 13:24:30 +0200

    Testar att kunna skicka och sända lokalt

---
+++ src/main/java/com/example/hakimlivs/rabbitmq/RabbitConsumer.java
@@ -0,0 +1,40 @@
+package com.example.hakimlivs.rabbitmq;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.QueueingConsumer;
+import org.springframework.web.bind.annotation.PostMapping;
+
+public class RabbitConsumer {
+    @PostMapping(path = "/rabbitReceice")
+    public String receive() throws Exception {
+        String uri = System.getenv("CLOUDAMQP_URL");
+        if (uri == null) uri = "amqp://guest:guest@localhost";
+
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setUri(uri);
+        factory.setPort(5672);
+        factory.setRequestedHeartbeat(30);
+        factory.setConnectionTimeout(30);
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.queueDeclare("hello", false, false, false, null);
+        String message = "Hello CloudAMQP!";
+//        channel.basicPublish("", "hello", null, message.getBytes());
+//        System.out.println(" [x] Sent '" + message + "'");
+
+        QueueingConsumer consumer = new QueueingConsumer(channel);
+        channel.basicConsume("hello", true, consumer);
+
+        while (true) {
+            QueueingConsumer.Delivery delivery = consumer.nextDelivery();
+            message = new String(delivery.getBody());
+            System.out.println(" [x] Received '" + message + "'");
+            return message;
+        }
+    }
+
+
+}
