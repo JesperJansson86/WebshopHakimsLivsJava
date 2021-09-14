039fb3522798a60c49af14a9af9d66727c35c1c0
Katri <suntribe2020@gmail.com>
2021-09-14 17:27:13 +0200

    Refactored and improved create customer test

--- src/test/java/com/example/hakimlivs/CustomerControllerTest.java
+++ src/test/java/com/example/hakimlivs/CustomerControllerTest.java
@@ -21,30 +21,57 @@ class CustomerControllerTest {
 
     @Autowired
     private WebTestClient webTestClient;
-    private final static String TEST_ID = "" + System.currentTimeMillis();
+
+    private static final String TEST_ID = "" + System.currentTimeMillis();
+    private static final String FIRST_NAME = "firstname";
+    private static final String LAST_NAME = "lastname";
+    private static final String EMAIL = "email";
+    private static final String PASSWORD = "password";
+    private static final String PHONE = "phone";
+    private static final String ADDRESS = "address";
+    private static final String AREA_CODE = "areaCode";
+    private static final String CITY = "city";
+    private static final String ADMIN_STATUS = "adminStatus";
+    private static final String LOYAL_CUSTOMER = "loyalCustomer";
+    private static final String CUSTOMER_ID = "id";
 
     @Test
     void shouldBeAbleToCreateUser() throws JSONException {
-        String signupBody = "{\n" +
-                "        \"firstname\":\"firstname" + TEST_ID + "\",\n" +
-                "        \"lastname\":\"lastname" + TEST_ID + "\",\n" +
-                "        \"email\":\"email" + TEST_ID + "\",\n" +
-                "        \"password\":\"password" + TEST_ID + "\",\n" +
-                "        \"phone\":\"phone" + TEST_ID + "\",\n" +
-                "        \"address\":\"address" + TEST_ID +"\",\n" +
-                "        \"areaCode\":\"areaCode" + TEST_ID + "\",\n" +
-                "        \"city\":\"city" + TEST_ID + "\"\n" +
-                "}";
+        //Create a customer
+        JSONObject body = generateCustomerBody();
+        createNewCustomer(body);
 
+        //Find and get the created customer
+        String allCustomersAsJsonArray = getAllCustomers();
+        JSONObject testCustomer = getCreatedTestCustomer(allCustomersAsJsonArray);
+
+        //assert correct customer data
+        assertNotNull(testCustomer);
+        assertEquals(FIRST_NAME + TEST_ID, testCustomer.getString("firstName")); //Change in CustomerDTO for consistency getters and Json keys
+        assertEquals(LAST_NAME + TEST_ID, testCustomer.getString("lastName")); //Same as above
+        assertEquals(EMAIL + TEST_ID, testCustomer.getString(EMAIL));
+        assertEquals(PHONE + TEST_ID, testCustomer.getString("phoneNumber")); //Same as above
+        assertNotNull(testCustomer.getString(ADDRESS));
+        assertNotNull(testCustomer.getString(PASSWORD));
+        assertFalse(testCustomer.getBoolean(ADMIN_STATUS));
+        assertFalse(testCustomer.getBoolean(LOYAL_CUSTOMER));
+
+        //Remove created testCustomer from database
+        deleteCustomer(testCustomer.getString(CUSTOMER_ID));
+    }
+
+    private void createNewCustomer(JSONObject body) {
         webTestClient.post()
                 .uri("api/customer/add")
                 .contentType(MediaType.APPLICATION_JSON)
-                .bodyValue(signupBody)
+                .bodyValue(body.toString())
                 .exchange()
                 .expectStatus()
                 .is2xxSuccessful();
+    }
 
-        String s = webTestClient.get()
+    private String getAllCustomers() {
+        return webTestClient.get()
                 .uri("api/customer/all")
                 .exchange()
                 .expectStatus()
@@ -52,25 +79,34 @@ class CustomerControllerTest {
                 .returnResult(String.class)
                 .getResponseBody()
                 .blockFirst();
-
-        JSONObject testUser = null;
-        testUser = getTestCreatedUser(s, testUser);
-
-        System.out.println(testUser);
-        assertNotNull(testUser);
-        assertTrue(testUser.getString("firstName").endsWith(TEST_ID));
-        assertTrue(testUser.getString("lastName").endsWith(TEST_ID));
-        assertTrue(testUser.getString("email").endsWith(TEST_ID));
-        //assertTrue(testUser.getString("password").endsWith(TEST_ID));
-        assertTrue(testUser.getString("phoneNumber").endsWith(TEST_ID));
-        assertTrue(testUser.getString("email").endsWith(TEST_ID));
     }
 
-    private JSONObject getTestCreatedUser(String s, JSONObject testUser) throws JSONException {
-        JSONArray allUsers = new JSONArray(s);
+    private void deleteCustomer(String customerId) {
+        webTestClient.delete()
+                .uri("api/customer/deleteById?id=" + customerId)
+                .exchange()
+                .expectStatus()
+                .is2xxSuccessful();
+    }
+
+    private JSONObject generateCustomerBody() throws JSONException {
+        JSONObject body = new JSONObject();
+        body.put(FIRST_NAME, FIRST_NAME + TEST_ID);
+        body.put(LAST_NAME, LAST_NAME + TEST_ID);
+        body.put(EMAIL, EMAIL + TEST_ID);
+        body.put(PASSWORD, PASSWORD + TEST_ID);
+        body.put(PHONE, PHONE + TEST_ID);
+        body.put(ADDRESS, ADDRESS + TEST_ID);
+        body.put(AREA_CODE, AREA_CODE + TEST_ID);
+        body.put(CITY, CITY + TEST_ID);
+        return body;
+    }
+
+    private JSONObject getCreatedTestCustomer(String allUsersAsJsonArray) throws JSONException {
+        JSONArray allUsers = new JSONArray(allUsersAsJsonArray);
         for (int i = 0; i < allUsers.length() ; i++) {
             JSONObject user = allUsers.getJSONObject(i);
-            if (user.getString("firstName").endsWith(TEST_ID)) {
+            if (user.getString(EMAIL).endsWith(TEST_ID)) {
                 return user;
             }
         }
